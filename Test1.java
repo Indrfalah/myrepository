c3a8a0693d07d7be26486f47fd57bf4069cb13c9
indrafallah <indrafallah@yahoo.com>
2015-09-28 07:05:37 +0700

    Tugas 3 PRPLL

---
+++ Test1.java
@@ -0,0 +1,11 @@
+public class Test1 {
+	public static void main (String[]args) {
+		System.out.println("What's wrong with program?");
+	}
+}
+
+class TestAnother1{
+	public static void main (String [] args) {
+		System.out.println("What's wrong with this program?");
+	}
+}
---
+++ TestGreeting.java
@@ -0,0 +1,12 @@
+public class TestGreeting {
+	public static void main (String [] args) {
+		Greeting hello = new Greeting();
+		hello.Greeting();
+	}
+}
+
+class Greeting {
+	public void Greeting() {
+		System.out.println("hi");
+	}
+}
